31c425d3e22caa7568296a1a507df433f0b5cba5
Armaan Thapar <armaant.08@gmail.com>
2026-03-22 10:52:52 -0400

    Further refining loading function logic, and cleaning up test code for loading into BigQuery

--- src/financial_data_pipeline/load_bq.py
+++ src/financial_data_pipeline/load_bq.py
@@ -6,10 +6,10 @@ from financial_data_pipeline.config import BQ_PROJECT_ID, BQ_DATASET_ID
 def bq_table(table_name: str) -> str:
     return f"{BQ_PROJECT_ID}.{BQ_DATASET_ID}.{table_name}"
 
-def load_parquet_to_bigquery(parquet_path: Path, destination_table: str, symbol: str, mode: str="WRITE_APPEND") -> None:
+def load_parquet_to_bigquery(parquet_path: Path, destination_table: str, mode: str="WRITE_APPEND") -> None:
     client = bigquery.Client(project = BQ_PROJECT_ID)
     df = pd.read_parquet(parquet_path)
-    
+
     job = client.load_table_from_dataframe(
         df,
         destination_table,
--- tests/test_load_bq.py
+++ tests/test_load_bq.py
@@ -1,9 +1,9 @@
-from financial_data_pipeline.load_bq import load_parquet_to_bigquery
-from pathlib import Path
-symbol = "TSLA"
-bars_dir = Path(r"C:\Users\armaa\OneDrive\Documents\GitFiles\financial-data-pipeline-project\data\raw\polygon\bars")
-input_file = bars_dir / symbol / "bars.parquet"
+from financial_data_pipeline.load_bq import load_parquet_to_bigquery, bq_table
+from financial_data_pipeline.config import BARS_BASE_DIR
 
-table_id = "market-data-pipeline-490602.marketdatapipeline.daily_bars"
-project_id = "market-data-pipeline-490602"
-load_parquet_to_bigquery(input_file, table_id, project_id, symbol)
+symbol = "TSLA"
+input_file = BARS_BASE_DIR / symbol / "bars.parquet"
+table_name = "daily_bars"
+table_id = bq_table(table_name)
+
+load_parquet_to_bigquery(input_file, table_id, mode="WRITE_TRUNCATE")
