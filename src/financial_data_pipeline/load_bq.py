40659888c25763f13f6076326a7ef149265afad4
Armaan Thapar <armaant.08@gmail.com>
2026-03-22 10:33:18 -0400

    Correcting loading function - force use of env variable for project_id and cleaning up variables

--- src/financial_data_pipeline/load_bq.py
+++ src/financial_data_pipeline/load_bq.py
@@ -6,16 +6,16 @@ from financial_data_pipeline.config import BQ_PROJECT_ID, BQ_DATASET_ID
 def bq_table(table_name: str) -> str:
     return f"{BQ_PROJECT_ID}.{BQ_DATASET_ID}.{table_name}"
 
-def load_parquet_to_bigquery(parquet_path: Path, bq_table_: str, project_id: str, symbol: str, mode: str="WRITE_APPEND") -> None:
-    client = bigquery.Client(project = project_id)
+def load_parquet_to_bigquery(parquet_path: Path, destination_table: str, symbol: str, mode: str="WRITE_APPEND") -> None:
+    client = bigquery.Client(project = BQ_PROJECT_ID)
     df = pd.read_parquet(parquet_path)
 
     df['symbol'] = symbol
     job = client.load_table_from_dataframe(
         df,
-        bq_table_,
+        destination_table,
         job_config=bigquery.LoadJobConfig(write_disposition=mode),
     )
     job.result()
-    print(f"Loaded {len(df)} rows to {bq_table_}")
+    print(f"Loaded {len(df)} rows to {destination_table}")
 
