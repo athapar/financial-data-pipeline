1b83192d2f4d9ad64f7676d0852e1fbf5c85864c
Armaan Thapar <armaant.08@gmail.com>
2026-04-01 16:03:28 -0400

    adding rolling volatility metrics from daily returns

--- warehouse/models/marts/mart_daily_returns.sql
+++ warehouse/models/marts/mart_daily_returns.sql
@@ -1,6 +1,27 @@
-select
-    symbol,
-    t,
-    close_price,
-    close_price / lag(close_price) over (partition by symbol order by t) - 1 as daily_return
+with returns AS (
+    select
+        ticker,
+        composite_figi,
+        price_date,
+        close_price,
+        close_price / lag(close_price) over (partition by composite_figi order by price_date) - 1 as daily_return
 from {{ ref('fact_daily_prices') }}
+)
+
+select
+    ticker,
+    composite_figi,
+    price_date,
+    close_price,
+    daily_return,
+    stddev(daily_return) OVER (
+                                PARTITION BY composite_figi
+                                ORDER BY price_date
+                                ROWS BETWEEN 19 PRECEDING and CURRENT ROW
+    ) AS volatility_20d,
+    stddev(daily_return) OVER (
+                                PARTITION BY composite_figi
+                                ORDER BY price_date
+                                ROWS BETWEEN 59 PRECEDING and CURRENT ROW
+    ) AS volatility_60d
+from returns
