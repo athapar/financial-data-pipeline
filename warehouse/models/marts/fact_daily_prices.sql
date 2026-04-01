c20bc1a59789213935b16dad8cdf492378085856
Armaan Thapar <armaant.08@gmail.com>
2026-04-01 16:02:21 -0400

    refactor marts: use scd2 security master for point in time pricing

--- warehouse/models/marts/fact_daily_prices.sql
+++ warehouse/models/marts/fact_daily_prices.sql
@@ -1,19 +1,79 @@
-with ranked as (
+with 
+    -- Cast bars data stored to staging table
+    bars as (  
+        SELECT  
+            Date(t) as price_date,
+            symbol,
+            o,
+            h,
+            l,
+            c,
+            v,
+            vw,
+            n
+
+        FROM {{ ref('stg_daily_bars') }}
+    ),
+    
+    -- Join bars to SCD2 security master and get composite_figi
+    bars_with_security as (
+        SELECT
+            m.composite_figi, 
+            b.*
+
+        FROM bars b INNER JOIN {{ ref('int_security_master_scd2') }} m
+        ON b.symbol = m.ticker AND m.dbt_valid_to is null
+    ),
+    
+    -- Adjust historic prices using adjustment factors calculated from splits data
+    bars_with_factors as (
+        SELECT
+            b.composite_figi as composite_figi,
+            b.symbol as ticker,
+            b.price_date as price_date,
+            b.o / s.adjustment_factor AS open_price,
+            b.h / s.adjustment_factor AS high_price,
+            b.l / s.adjustment_factor AS low_price,
+            b.c / s.adjustment_factor AS close_price,
+            b.vw / s.adjustment_factor AS vwap,
+            b.v AS volume,        -- not a price, no adjustment
+            b.n AS trade_count    -- not a price, no adjustment
+
+        FROM bars_with_security b INNER JOIN {{ ref('int_split_adjustment_factors') }} s
+        ON b.composite_figi = s.composite_figi and b.price_date = s.price_date
+
+    ),
+
+    -- dedupe data
+    ranked as (
     select
-        symbol,
-        t,
-        c as close_price,
-        v as volume,
+        composite_figi,
+        ticker,
+        price_date,
+        open_price,     -- already adjusted in bars_with_factors
+        high_price,     -- already adjusted in bars_with_factors
+        low_price,      -- already adjusted in bars_with_factors
+        close_price,    -- already adjusted in bars_with_factors
+        vwap,           -- already adjusted in bars_with_factors
+        volume,       
+        trade_count,    
         row_number() over (
-            partition by symbol, t
-            order by t desc
+            partition by composite_figi, price_date
+            order by price_date desc
         ) as rn
-    from {{ ref('stg_daily_bars') }}
+    from bars_with_factors
 )
+
 select
-    symbol,
-    t,
-    close_price,
-    volume
+    composite_figi,
+    ticker,
+    price_date,
+    open_price,
+    high_price,
+    low_price,
+    close_price, 
+    vwap,
+    volume,
+    trade_count
 from ranked
 where rn = 1
